21627a5ac3515733099f408e956e9f261bc7372c
yushchuk6 <yushchuk6@gmail.com>
2021-06-29 15:36:23 +0300

    code cleanup

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,17 @@
 from django import forms
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 
-class UserAdminForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = "__all__"
+# class UserAdminForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = "__all__"
 
 
-class UserAdmin(admin.ModelAdmin):
-    form = UserAdminForm
+# class UserAdmin(admin.ModelAdmin):
+#     form = UserAdminForm
 
 
 admin.site.register(User, UserAdmin)
